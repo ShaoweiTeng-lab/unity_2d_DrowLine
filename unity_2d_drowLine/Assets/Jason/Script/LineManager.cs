0ef125edf6c2fcba84076784cdad535417634f9e
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2021-04-17 19:58:19 +0800

    整理code

--- unity_2d_drowLine/Assets/Jason/Script/LineManager.cs
+++ unity_2d_drowLine/Assets/Jason/Script/LineManager.cs
@@ -17,22 +17,17 @@ public class LineManager : MonoBehaviour
     EdgeCollider2D edgCollider;
     [SerializeField]
     List<Vector2> fingerpointsPos;//點擊的位置
-    
-
     public Color m_Color;
-    public Rigidbody2D playerRig;
-    public Toggle checkUsegravity;
-    public Toggle checkUsePhysicsMaterial;
-    public PhysicsMaterial2D m_PhysicsMaterial2D;
-    public Button ReturnScence;
-    [Range(0, 1.5f)]
-    public float PhysicsMaterialRange;
-    public Slider m_PhysicsMaterialRange;
-    public Text SendPhysicsMaterial_text;
-    private void Start()
-    {
-        ReturnScence.onClick.AddListener(()=>ReturnOnclick());
-    }
+    //public Rigidbody2D playerRig;
+    //public Toggle checkUsegravity;
+    //public Toggle checkUsePhysicsMaterial;
+    //public PhysicsMaterial2D m_PhysicsMaterial2D;
+    //public Button ReturnScence;
+    //[Range(0, 1.5f)]
+    //public float PhysicsMaterialRange;
+    //public Slider m_PhysicsMaterialRange;
+    //public Text SendPhysicsMaterial_text;
+   
     // Update is called once per frame
     void Update()
     {
@@ -41,7 +36,7 @@ public class LineManager : MonoBehaviour
     void GameLoop()
     {
         LineRenderUpdate();
-        PhysicUpdate();
+        
     }
     #region LinRenderDraw
     void CreateLIne()
@@ -82,29 +77,6 @@ public class LineManager : MonoBehaviour
     }
     #endregion
 
-
-    #region PhysicsMaterial
-    void ReturnOnclick()
-    {
-        SceneManager.LoadScene("LineDraw");
-    }
-    void PhysicUpdate()
-    {
-        if (checkUsegravity.isOn)
-            playerRig.simulated = true;
-        else
-            playerRig.simulated = false;
-        if (checkUsePhysicsMaterial.isOn)
-            playerRig.sharedMaterial = m_PhysicsMaterial2D;
-        else
-            playerRig.sharedMaterial = null;
-        PhysicsMaterialRange = m_PhysicsMaterialRange.value * 1.5f;
-        SendPhysicsMaterial_text.text = string.Format("彈力指數: {0}", PhysicsMaterialRange.ToString("0.0"));
-        m_PhysicsMaterial2D.bounciness = PhysicsMaterialRange;
-
-
-    }
-    #endregion
 
 
 }
---
+++ unity_2d_drowLine/Assets/Jason/Script/PhysisScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class PhysisScript : MonoBehaviour
+{
+    public Color m_Color;
+    public Rigidbody2D playerRig;
+    public Toggle checkUsegravity;
+    public Toggle checkUsePhysicsMaterial;
+    public PhysicsMaterial2D m_PhysicsMaterial2D;
+    public Button ReturnScence;
+    [Range(0, 1.5f)]
+    public float PhysicsMaterialRange;
+    public Slider m_PhysicsMaterialRange;
+    public Text SendPhysicsMaterial_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ReturnScence.onClick.AddListener(() => ReturnOnclick());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PhysicUpdate();
+    }
+    #region PhysicsMaterial
+    void ReturnOnclick()
+    {
+        SceneManager.LoadScene("LineDraw");
+    }
+    void PhysicUpdate()
+    {
+        if (checkUsegravity.isOn)
+            playerRig.simulated = true;
+        else
+            playerRig.simulated = false;
+        if (checkUsePhysicsMaterial.isOn)
+            playerRig.sharedMaterial = m_PhysicsMaterial2D;
+        else
+            playerRig.sharedMaterial = null;
+        PhysicsMaterialRange = m_PhysicsMaterialRange.value * 1.5f;
+        SendPhysicsMaterial_text.text = string.Format("彈力指數: {0}", PhysicsMaterialRange.ToString("0.0"));
+        m_PhysicsMaterial2D.bounciness = PhysicsMaterialRange;
+
+
+    }
+    #endregion
+
+
+}
