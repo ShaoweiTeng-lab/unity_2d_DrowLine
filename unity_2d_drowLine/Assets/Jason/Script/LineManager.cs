63cd7ebe537de760e5a60f7f38a62e13c15a71b8
Jason_Teng <50354880+sdonline13@users.noreply.github.com>
2021-04-17 11:35:25 +0800

    完成換線及碰撞器之設定

--- unity_2d_drowLine/Assets/Jason/Script/LineManager.cs
+++ unity_2d_drowLine/Assets/Jason/Script/LineManager.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LineManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+
+    public GameObject linePrefeb;
+    [SerializeField]
+     GameObject currentLine;
+    [SerializeField]
+    LineRenderer line_Render;
+    [SerializeField]
+    EdgeCollider2D edgCollider;
+    [SerializeField]
+    List<Vector2> fingerpointsPos;
+
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            CreateLIne();//先創第一點
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector2 nowfingerpoint= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector2.Distance(nowfingerpoint, fingerpointsPos[fingerpointsPos.Count-1])>.1f) //距離上一點的距離
+                UpdateLine(nowfingerpoint);
+        }
+    }
+    void CreateLIne() {
+
+        currentLine = Instantiate(linePrefeb, Vector3.zero, Quaternion.identity);
+        line_Render = currentLine.GetComponent<LineRenderer>();
+        edgCollider = currentLine.GetComponent<EdgeCollider2D>();
+         
+        fingerpointsPos.Clear();
+        fingerpointsPos.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        fingerpointsPos.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        line_Render.SetPosition(0, fingerpointsPos[0]);
+        line_Render.SetPosition(1, fingerpointsPos[1]);
+        edgCollider.points= fingerpointsPos.ToArray();
+    }
+    void UpdateLine(Vector2 newFingerPos) {
+        fingerpointsPos.Add(newFingerPos);
+        line_Render.positionCount++;
+        line_Render.SetPosition(line_Render.positionCount - 1, newFingerPos);
+        edgCollider.points = fingerpointsPos.ToArray();
+
     }
 }
